440f997873d0260d9510ce7e731b42dbe07159e6
Bro-Maankah <ramzaan1440@gmail.com>
2024-11-07 19:53:21 +0300

    first commit

---
+++ Javascript/Js_Basics/Js_Exercise2/main.js
@@ -0,0 +1,12 @@
+// JavaScript Exercise #2 : Comparison Operators
+
+let a = 15;
+let b = "15";
+console.log(a == b);  // Output: true
+console.log(a === b); // Output: false
+console.log(a != b);  // Output: false
+console.log(a !== b); // Output: true
+console.log(a > b);   // Output: false
+console.log(a < b);   // Output: false
+console.log(a >= b);  // Output: true
+console.log(a <= b);  // Output: true
